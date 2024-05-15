371b621ea945c8ea206a99a3d84beb02aeaf3b09
jemunoz <jemunoz@106-28.info2>
2024-05-15 08:22:21 +0200

    Listado Viajes

--- src/metodos/ArrayViajes.java
+++ src/metodos/ArrayViajes.java
@@ -1,13 +1,19 @@
 package metodos;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Clase que contreda el listado de viajes disponibles con sus respectivos datos.
+ * Clase que contendra el listado de viajes disponibles con sus respectivos datos.
  */
 public class ArrayViajes {
 	
-	public static void main(String[] args) {
-		
-		System.out.println("----------  Listado de Viajes disponibles  ----------");
-		
+static List<Viaje> listaViajes = new ArrayList<>();
+
+public static void mostrarViajes() {
+	for ( Viaje viaje : listaViajes) {
+		System.out.println(viaje);
 	}
+}
+
 }
